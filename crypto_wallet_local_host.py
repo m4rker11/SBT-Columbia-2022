43a4d5e3610ea1c8160cd1e220062fb4d268b191
gabeferreri71 <95319421+gabeferreri71@users.noreply.github.com>
2022-05-24 20:54:51 -0400

    Add files via upload
    Local ganache backup

---
+++ crypto_wallet_local_host.py
@@ -0,0 +1,73 @@
+# Cryptocurrency Wallet
+################################################################################
+
+# This file contains the Ethereum transaction functions that you have created throughout this module’s lessons. By using import statements, you will integrate this `crypto_wallet.py` Python script into the Fintech Finder interface program that is found in the `fintech_finder.py` file.
+
+################################################################################
+# Imports
+import os
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+from bip44 import Wallet
+from web3 import Account
+from web3 import middleware
+from web3.gas_strategies.time_based import medium_gas_price_strategy
+
+################################################################################
+# Wallet functionality
+
+def generate_account():
+    """Create a digital wallet and Ethereum account from a mnemonic seed phrase."""
+    # Fetch mnemonic from environment variable.
+    mnemonic = os.getenv("MNEMONIC")
+
+    # Create Wallet Object
+    wallet = Wallet(mnemonic)
+
+    # Derive Ethereum Private Key
+    private, public = wallet.derive_account("matic")
+
+    # Convert private key into an Ethereum account
+    account = Account.privateKeyToAccount(private)
+
+    return account
+
+def get_balance(w3, address):
+    """Using an Ethereum account address access the balance of Ether"""
+    # Get balance of address in Wei
+    wei_balance = w3.matic.get_balance(address)
+
+    # Convert Wei value to ether
+    matic = w3.fromWei(wei_balance, "matic")
+
+    # Return the value in ether
+    return matic
+
+
+def send_transaction(w3, account, to, wage):
+    """Send an authorized transaction to the Ganache blockchain."""
+    # Set gas price strategy
+    w3.matic.setGasPriceStrategy(medium_gas_price_strategy)
+
+    # Convert eth amount to Wei
+    value = w3.toWei(wage, "matic")
+
+    # Calculate gas estimate
+    gasEstimate = w3.matic.estimateGas({"to": to, "from": account.address, "value": value})
+
+    # Construct a raw transaction
+    raw_tx = {
+        "to": to,
+        "from": account.address,
+        "value": value,
+        "gas": gasEstimate,
+        "gasPrice": 0,
+        "nonce": w3.matic.getTransactionCount(account.address)
+    }
+
+    # Sign the raw transaction with ethereum account
+    signed_tx = account.signTransaction(raw_tx)
+
+    # Send the signed transactions
+    return w3.matic.sendRawTransaction(signed_tx.rawTransaction)
